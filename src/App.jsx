6ec990d6142bcf476ec37a9b2bd142a2958fd6bc
Lucas Shahbazi <lucasshahbazi@yahoo.de>
2025-02-17 14:49:45 +0100

    Lifted states

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
 import { Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import ExpenseForm from "./components/ExpenseForm";
+// import IncomeForm 
 import './App.css'
 
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
-
 import ErrorPage from './pages/ErrorPage'
-function App() {
+
+
+const ParentComponent = () => {
+  const [expenses, setExpenses] = useState([]);
+  const [incomes, setIncomes] = useState([]);
+
+  useEffect(() => {
+    const fetchExpenses = async () => // ...
+    const fetchIncomes = async () => //...
+
+    fetchExpenses();
+    fetchIncomes();
+  },[]);
+
+  const handleExpenseAdded = (newExpense) => {
+    setExpenses([...expenses, newExpense]);
+  };
+
+  const handleIncomeAdded = (newIncome) => {
+    setIncomes = ([...incomes, newIncome]);
+  };
 
   return (
     <>
-      <Navbar/>
-      <Sidebar/>
+      <div>
+        <ExpenseForm onExpenseAdded={handleExpenseAdded} />
+        <IncomeForm onIncomeAdded={handleIncomeAdded} />
+      </div>
+    <Navbar/>
+    <Sidebar/>
 
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='*' element={<ErrorPage/>}/>
-      </Routes>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='*' element={<ErrorPage/>}/>
+    </Routes>
 
-      <Footer></Footer>
-    </>
-  )
-}
-
-export default App
+    <Footer/>
+  </>
+  );
+};
