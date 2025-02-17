9be4e769e5442f472803e536a94d08101a22c12c
aoge2716 <aoge2716@gmail.com>
2025-02-17 12:44:52 +0100

    init firebase

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,36 @@
+import { db } from "../firebase"
+import { useEffect, useState } from "react"
+import { collection, getDocs } from "firebase/firestore"
+
 export default function Home(){
+    const[expenses, setExpenses] = useState([]);
+
+    useEffect(()=>{
+        async function fetchExpenses(){
+            try{
+                const querySnapshot = await getDocs(collection(db, "expenses"));
+                const expenses = querySnapshot.docs.map(expense => ({
+                    id:expense.id,
+                    ...expense.data()
+                }));
+                setExpenses(expenses);
+            } catch (error) {
+                console.error("Error fetch expense: ", error)
+            }
+        } 
+        fetchExpenses();
+    }, [])
+
     return(
         <div className="home">
             
             <h1>Home</h1>
+            <ul>
+                {expenses.map(el=>{
+                    {console.log(el)}
+                })}
+            </ul>
+            
             
         </div>
     )
